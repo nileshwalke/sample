b81d65a845d771d4273c741543990ecdda773bbb
cmuser <nilesh.walke@culturemachine.in>
2017-12-28 12:47:02 +0530

    new change

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-nilesh walke software engineer
-
-culture machine media pvt ltd.
-=======
 nilesh walke software engineer culture
->>>>>>> Stashed changes
+culture machine media
